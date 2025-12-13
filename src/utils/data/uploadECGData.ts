042c3ad206a9e9637059f5f170ac7aa159c714db
Zaina Khan <zainakhan8484@gmail.com>
2025-12-12 17:33:45 -0800

    Adding files in data folder

---
+++ src/utils/data/uploadECGData.ts
@@ -0,0 +1,9 @@
+export const allowedFormats = [".csv", ".mat", ".txt"];
+
+export const fileRequirements: string[] = [
+  "CSV files with time-voltage columns",
+  "MAT files from MATLAB",
+  "TXT files with ECG data",
+  "Sampling rate: 360 Hz recommended",
+  "Duration: 10-30 seconds",
+];
